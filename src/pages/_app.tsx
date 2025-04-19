9db261f7b884f70b962298f087e1f8220b4ddd22
jameswright1562 <114619732+jameswright1562@users.noreply.github.com>
2025-04-19 15:43:11 +0100

    Set page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../app/globals.css";
 import Navbar from "@/components/navbar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>GitHub Actions Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <main className="p-6">
         <Component {...pageProps} />
